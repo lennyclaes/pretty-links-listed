1a80cd8aae70ea8ea55daf3706a226278cc03f82
Lenny Claes <lenny.claes@gmail.com>
2021-12-02 23:41:37 +0100

    Added acces for Author

--- wp_pl_listed.php
+++ wp_pl_listed.php
@@ -4,17 +4,28 @@
  * Plugin name: Pretty Links Listed
  * Description: A plugin to list the URLs created by Pretty Links to Authors
  * Author: Lenny Claes
- * Version: 1.0.0
+ * Version: 1.0.1
  */
 
 add_action('admin_menu', 'pl_listed_setup_menu');
 
 function pl_listed_setup_menu()
 {
-    add_menu_page('Pretty Links List', 'Pretty Links Listed', 'manage_options', 'pl_listed', 'pl_listed_init');
+    add_menu_page('Pretty Links List', 'Pretty Links Listed', 'read_pl', 'pl_listed', 'pl_listed_init');
+    // $role = get_role('Author');
+    // $role->add_cap('read_pl', true);
 }
 
 add_action('admin_init', 'load_scripts');
+
+function pl_listed_add_capabilities() {
+    $role = get_role('author');
+    $role->add_cap('read_pl', true);
+    $role = get_role('administrator');
+    $role->add_cap('read_pl', true);
+}
+
+add_action('init', 'pl_listed_add_capabilities', 11);
 
 function load_scripts() {
     wp_register_style('main', plugins_url('app/css/main.css', __FILE__));
